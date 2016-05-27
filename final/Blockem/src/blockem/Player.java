a32f2c9e628bea9e553758be0c837b6ce0475069
Allie Warren <warrena@carleton.edu>
2016-05-27 15:58:05 +0000

    added javadoc comments to Player

--- final/Blockem/src/blockem/Player.java
+++ final/Blockem/src/blockem/Player.java
@@ -4,7 +4,9 @@ import javafx.scene.image.ImageView;
 import java.util.ArrayList;
 
 /**
- * Created by pitkofskyj on 5/26/16.
+ * Player Class
+ * contains the different attributes of the players
+ * Created on 5/26/16.
  */
 public class Player {
 
@@ -15,6 +17,13 @@ public class Player {
     boolean passed;
 
 
+    /**
+    * Sole constructor to initialize player. Initializes 
+    * the starting score of the game and creates the player's initial
+    * list/repo of pieces
+    * @param name the String name of the player
+    * @param avatar the avatar image for the player
+    */
     public Player(String name, ImageView avatar) {
         score = 89;
         passed = false;
@@ -25,34 +34,73 @@ public class Player {
     }
 
 
+    /** 
+    * Returns a Boolean that is true if the player has
+    * passed at any point in the game, and is false
+    * if the player has never passed in this game.
+    * @return true or false
+    */
     public boolean getPassed() {
         return passed;
     }
 
-    public void setPassed(boolean pass) {
+    /** 
+    * Sets the player’s passed Boolean to true once
+    * the player passes in the game
+    */
+   public void setPassed() {
         passed = true;
     }
 
+    /** 
+    * Returns the avatar image that refers to this player
+    * @return  image of the avatar
+    */
     public ImageView getAvatar() {
         return avatar;
     }
 
+    /** 
+    * Returns the list of pieces that the player currently has in
+    * their repository. This list includes all of the pieces which the
+    * player has not yet played in the game.
+    * @return ArrayList of Pieces
+    */
     public ArrayList<Piece> getPieces() {
         return myPieces;
     }
 
+    /** 
+    * Returns the name of this player
+    * @return a string of the player’s name
+    */
     public String getName() {
         return name;
     }
-
+    
+    /** 
+    * Returns the current score of the player
+    * @return an int score
+    */
     public int getScore() {
         return score;
     }
 
+    /** 
+    * Returns the current score of the player
+    * @return an int score
+    */
     public void updateScore(int pieceSize) {
         score = score - pieceSize;
     }
 
+    /** 
+    * Returns true if the piece that the player wants to play
+    * is in their list/repository of pieces, meaning the player
+    * has not already played that piece 
+    * @param playedPiece the Piece object that the player just played
+    * @return true or false
+    */
     public boolean hasPiece(Piece playedPiece) {
         if (myPieces.contains(playedPiece)) {
             return true;
@@ -61,11 +109,22 @@ public class Player {
         }
     }
 
+    /** 
+    * Removes the piece that the player just played
+    * from their list/repository of pieces.
+    * @param piecePassed the Piece object that the player just played
+    */
     public void removePiece(Piece piecePassed) {
         myPieces.remove(piecePassed);
     }
 
-
+    /** 
+    * Returns the initial list of all the pieces that the player
+    * starts with at the beginning of the game. This method
+    * initialized the 21 Piece objects with their relative coordinates
+    * (which describe the shape of each piece)
+    * @return ArrayList containing 21 Pieces
+    */
     public ArrayList<Piece> createAllPieces() {
         //Piece l4 = new Piece([0, 0],[1, 1],[1, 2]);
         //n, v5, t5, u, l5, i5, z5, y, w, p, x, f, z4, i4, l4, o, t4, i3, v3, 2, 1
