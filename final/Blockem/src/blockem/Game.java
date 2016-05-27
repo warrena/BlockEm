2803361d9ea56c7fb0d8a89a7c4a0f9a732545f9
Allie Warren <warrena@carleton.edu>
2016-05-27 16:12:21 +0000

    added javadoc comments to Game

--- final/Blockem/src/blockem/Game.java
+++ final/Blockem/src/blockem/Game.java
@@ -10,7 +10,8 @@ import blockem.Player;
 import java.util.ArrayList;
 
 /**
- * Created by pitkofskyj on 5/26/16.
+ * Game class
+ * Created on 5/26/16.
  */
 public class Game extends Application {
     ArrayList<Player> players = new ArrayList<Player>();
@@ -19,16 +20,34 @@ public class Game extends Application {
     Board board;
 
 
+    /**
+    * sole constructor for this class,
+    * it initializes the players for the game, and set the current
+    * player to the first player in the list
+    */
     public Game(ArrayList<Player> players) {
         currentPlayer = players.get(0);
+        this.players = players;
     }
 
+    /** 
+    * Returns the player whose turn is next
+    * @return player
+    */
     public Player getNextPlayer() {
         //getNextPlayer - returns the next player, checks allPassed and calls gameOver if valid
 
         return null;
     }
 
+    /** 
+    * Checks whether the piece that the player played
+    * can be played at that location on the board (that the piece connects to the 
+    * corner of another piece from that player) and is a piece that the player has.
+    * If both of these conditions are true, it returns true.
+    * @param piece the piece that the player added to the board
+    * @return true if placement of piece is valid
+    */
     public boolean checkValidPiece(Piece piece) {
         if (board.checkValidPlacement() && currentPlayer.hasPiece(piece)) {
             return true;
@@ -37,7 +56,13 @@ public class Game extends Application {
         }
     }
 
-
+    /** 
+    * Checks whether the current player can select a square on the game board.
+    * The player cannot select squares where a piece has already been played
+    * or which is adjacent to one of their pieces.
+    * @param x, y the coordinates of the board square the player is trying to select
+    * @return true or false
+    */
     public boolean checkValidClick(int x, int y) {
         if (board.checkClick(x,y, currentPlayer)) {
             return true;
@@ -46,6 +71,11 @@ public class Game extends Application {
         }
     }
 
+    /** 
+    * Returns true if all of the players have passed; meaning that no
+    * player can play another piece and the game is over.
+    * @return true or false
+    */
     public boolean allPassed() {
         if (numberPassed < 4) {
             return false;
@@ -53,7 +83,13 @@ public class Game extends Application {
         return true;
     }
 
-    public void gameOver() {
+    /** 
+    * Returns the player that has won the game.
+    * This is called once the game is over, and determines the 
+    * winner by finding the player with the lowest score.
+    * @return winning player
+    */
+    public Player gameOver() {
         //still need to figure out ties
         Player winningPlayer;
         int winningScore = 0;
@@ -62,6 +98,7 @@ public class Game extends Application {
                 winningPlayer = player;
             }
         }
+        return winningPlayer;
     }
 
     @Override
