cc1e582ea5bc9428554dc89abb48290702eb6c8b
joshpitkofsky <pitkofsky.j@gmail.com>
2016-06-03 21:32:00 -0500

    Pictures work and score added

--- final/pong/src/blockem/Controller.java
+++ final/pong/src/blockem/Controller.java
@@ -21,13 +21,20 @@ public class Controller implements EventHandler<KeyEvent> {
     ArrayList<GridCell> clicks = new ArrayList<GridCell>();
 
 
-    @FXML private GridPane grid;
-    @FXML private GridPane playerView;
-    @FXML private GridPane pieceView;
-    @FXML private ImageView playerOneAvatar;
-    @FXML private ImageView playerTwoAvatar;
-    @FXML private ImageView playerThreeAvatar;
-    @FXML private ImageView playerFourAvatar;
+    @FXML
+    private GridPane grid;
+    @FXML
+    private GridPane playerView;
+    @FXML
+    private GridPane pieceView;
+    @FXML
+    private ImageView playerOneAvatar;
+    @FXML
+    private ImageView playerTwoAvatar;
+    @FXML
+    private ImageView playerThreeAvatar;
+    @FXML
+    private ImageView playerFourAvatar;
 
     private int score;
     private boolean paused;
@@ -46,10 +53,10 @@ public class Controller implements EventHandler<KeyEvent> {
         players.add(new Player("Jeremiah", "/pic.jpg", 0, 0, 255));
         players.add(new Player("Allie", "/pic.jpg", 0, 204, 0));
 
-        Image red = new Image("file:res/red.jpg");
+        Image red = new Image("file:@res/red.jpg");
         Image blue = new Image("file:@res/blue.jpg");
-        Image green = new Image("file:res/green.jpg");
-        Image purple = new Image("file:res/purple.jpg");
+        Image green = new Image("file:@res/green.jpg");
+        Image purple = new Image("file:@res/purple.jpg");
         this.playerOneAvatar = new ImageView();
         playerOneAvatar.setImage(red);
         this.playerTwoAvatar = new ImageView();
@@ -65,24 +72,22 @@ public class Controller implements EventHandler<KeyEvent> {
 
 
         // Creates all of the rows and columns
-        for(int i=0; i<20; i++){
+        for (int i = 0; i < 20; i++) {
             grid.getColumnConstraints().add(new ColumnConstraints(20));
             grid.getRowConstraints().add(new RowConstraints(20));
         }
 
         // creates all of the panes in the gridpane
-        for(int i=0; i<21; i++){
-            for(int j=0; j<21; j++){
+        for (int i = 0; i < 21; i++) {
+            for (int j = 0; j < 21; j++) {
                 final Pane cell = new Pane();
                 //cell.setStyle("-fx-background-color:yellow;");
                 grid.add(cell, i, j);
 
-                cell.setOnMouseClicked(new EventHandler<MouseEvent>()
-                {
+                cell.setOnMouseClicked(new EventHandler<MouseEvent>() {
                     @Override
-                    public void handle(MouseEvent arg0)
-                    {
-                        Pane src = (Pane)arg0.getSource();
+                    public void handle(MouseEvent arg0) {
+                        Pane src = (Pane) arg0.getSource();
                         handleClick(src);
                     }
                 });
@@ -113,6 +118,7 @@ public class Controller implements EventHandler<KeyEvent> {
 
     /**
      * Returns the player whose turn is next
+     *
      * @return player
      */
     public Player getNextPlayer() {
@@ -120,7 +126,7 @@ public class Controller implements EventHandler<KeyEvent> {
 
 
         int curIndex = players.indexOf(currentPlayer);
-        if(curIndex == 3) {
+        if (curIndex == 3) {
             currentPlayer = players.get(0);
         } else {
             currentPlayer = players.get(curIndex + 1);
@@ -133,11 +139,12 @@ public class Controller implements EventHandler<KeyEvent> {
      * can be played at that location on the board (that the piece connects to the
      * corner of another piece from that player) and is a piece that the player has.
      * If both of these conditions are true, it returns true.
+     *
      * @return true if placement of piece is valid
      */
     @FXML
     public void checkValidPiece() {
-        for(GridCell click: clicks) {
+        for (GridCell click : clicks) {
             String colorString = "-fx-background-color:" + currentPlayer.getColorString() + ";";
             click.getPane().setStyle(colorString);
         }
@@ -160,11 +167,12 @@ public class Controller implements EventHandler<KeyEvent> {
      * Checks whether the current player can select a square on the game board.
      * The player cannot select squares where a piece has already been played
      * or which is adjacent to one of their pieces.
+     *
      * @param x, y the coordinates of the board square the player is trying to select
      * @return true or false
      */
     public boolean checkValidClick(int x, int y) {
-        if (board.checkClick(x,y, currentPlayer)) {
+        if (board.checkClick(x, y, currentPlayer)) {
             return true;
         } else {
             return false;
@@ -174,6 +182,7 @@ public class Controller implements EventHandler<KeyEvent> {
     /**
      * Returns true if all of the players have passed; meaning that no
      * player can play another piece and the game is over.
+     *
      * @return true or false
      */
     public boolean allPassed() {
@@ -187,6 +196,7 @@ public class Controller implements EventHandler<KeyEvent> {
      * Returns the player that has won the game.
      * This is called once the game is over, and determines the
      * winner by finding the player with the lowest score.
+     *
      * @return winning player
      */
     public Player gameOver() {
